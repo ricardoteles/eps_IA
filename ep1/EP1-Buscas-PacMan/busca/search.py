c565569975dbbb49360715d8b7007b40a817d87d
ricardoteles <ricardo.oliveira.teles@gmail.com>
2016-08-29 09:41:32 -0300

    Busca profundidade, largura, heuristica e custo uniforme implementada

--- ep1/EP1-Buscas-PacMan/busca/search.py
+++ ep1/EP1-Buscas-PacMan/busca/search.py
@@ -84,7 +84,7 @@ def depthFirstSearch(problem):
 
     """
     coordAtual = problem.getStartState()
-    rotas = [[(problem.getStartState(),'',0)]]
+    rotas = [ [(problem.getStartState(),'',0)] ]
     visitados = []
 
     while not problem.isGoalState(coordAtual):                     
@@ -112,13 +112,46 @@ def depthFirstSearch(problem):
         resp.append(tuplas[1])
         custo += tuplas[2]
 
+    raw_input("Aperte a tecla")
     return resp
-
+    
+    # "*** YOUR CODE HERE ***"
+    # util.raiseNotDefined()
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    coordAtual = problem.getStartState()
+    rotas = [[(problem.getStartState(),'',0)]]
+    visitados = []
+
+    while not problem.isGoalState(coordAtual):                     
+        caminho = rotas.pop(0)
+        coordAtual = caminho[-1][0]
+
+        if coordAtual not in visitados:                            
+            novasRotas = []
+            visitados.append(coordAtual)
+
+            vizinhos = problem.getSuccessors(coordAtual)
+
+            for x in vizinhos:     
+                novoCaminho = caminho[:]
+                novoCaminho.append(x)
+
+                novasRotas.append(novoCaminho)                     
+
+            rotas =  rotas+novasRotas
+
+    resp = []
+    custo = 0
+
+    for tuplas in caminho[1:]:
+        resp.append(tuplas[1])
+        custo += tuplas[2]
+
+    raw_input("Aperte a tecla")
+    return resp
 
 def iterativeDeepeningSearch(problem):
     """
@@ -132,10 +165,46 @@ def iterativeDeepeningSearch(problem):
     "*** YOUR CODE HERE ***"
     util.raiseNotDefined()
 
+# Funcao auxiliar para ordenar a lista pelo custo
+def getKey(rotas):
+    return rotas[0]
+
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    coordAtual = problem.getStartState()
+    rotas = [[0, (problem.getStartState(),'',0)]]       # primeiro elem eh o custo
+    visitados = []
+
+    while not problem.isGoalState(coordAtual):                     
+        rotas = sorted(rotas, key=getKey)
+        caminho = rotas.pop(0)
+        coordAtual = caminho[-1][0]
+
+        if coordAtual not in visitados:                            
+            novasRotas = []
+            visitados.append(coordAtual)
+
+            vizinhos = problem.getSuccessors(coordAtual)
+
+            for x in vizinhos:     
+                novoCaminho = caminho[:]    
+                novoCaminho.append(x)
+                novoCaminho[0] += novoCaminho[-1][2]
+                novasRotas.append(novoCaminho)                     
+            
+            rotas =  novasRotas+rotas
+
+    resp = []
+    custo = 0
+
+    for tuplas in caminho[2:]:
+        resp.append(tuplas[1])
+        custo += tuplas[2]
+
+    raw_input("Aperte a tecla")
+    return resp    
+
 
 def nullHeuristic(state, problem=None):
     """
@@ -144,11 +213,45 @@ def nullHeuristic(state, problem=None):
     """
     return 0
 
+def manhattanDistance( xy1, xy2 ):
+    "Returns the Manhattan distance between points xy1 and xy2"
+    return abs( xy1[0] - xy2[0] ) + abs( xy1[1] - xy2[1] )
+
 def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    coordAtual = problem.getStartState()
+    rotas = [[0, (problem.getStartState(),'',0)]]       # primeiro elem eh o custo
+    visitados = []
 
+    while not problem.isGoalState(coordAtual):                     
+        rotas = sorted(rotas, key=getKey)
+        caminho = rotas.pop(0)
+        coordAtual = caminho[-1][0]
+
+        if coordAtual not in visitados:                            
+            novasRotas = []
+            visitados.append(coordAtual)
+
+            vizinhos = problem.getSuccessors(coordAtual)
+
+            for x in vizinhos:     
+                novoCaminho = caminho[:]    
+                novoCaminho.append(x)
+                novoCaminho[0] += (novoCaminho[-1][2]+manhattanDistance(coordAtual, (1,1)))
+                novasRotas.append(novoCaminho)                     
+            
+            rotas =  novasRotas+rotas
+
+    resp = []
+    custo = 0
+
+    for tuplas in caminho[2:]:
+        resp.append(tuplas[1])
+        custo += tuplas[2]
+
+    raw_input("Aperte a tecla")
+    return resp
 
 # Abbreviations
 bfs = breadthFirstSearch
